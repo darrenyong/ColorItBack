9f645a789fd778f7434441c0fd94a520d4f5ffb4
darrenyong <contact.darrenyong@gmail.com>
2019-03-15 16:36:51 -0700

    Update file structure

--- js/app.js
+++
@@ -1,6 +0,0 @@
-import Game from "./game"
-
-document.addEventListener("DOMContentLoaded", () => {
-  const canvas = document.getElementById("nonogram");
-  const ctx = canvas.getContext("2d");
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  context: __dirname,
   entry: './js/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  devtool: 'source-map'
 };
