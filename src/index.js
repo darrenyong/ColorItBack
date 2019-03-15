14b7e11584dc90b884499d84a560f4067f979555
darrenyong <contact.darrenyong@gmail.com>
2019-03-15 10:11:47 -0700

    Start setup of Canvas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import _ from 'lodash';
 
-function component() {
-  let element = document.createElement('div');
+let canvas = document.querySelector('canvas');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(canvas);
 
-  return element;
-}
+let ctx = canvas.getContext('2d');
+ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+ctx.fillRect(100, 100, 100, 100)
+ctx.fillStyle = "rgba(0, 0, 255, 0.5)";
+ctx.fillRect(400, 100, 100, 100)
+ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+ctx.fillRect(300, 300, 100, 100)
 
-document.body.appendChild(component());
+ctx.beginPath();
+ctx.moveTo(50, 300);
+ctx.lineTo(300, 100);
+ctx.lineTo(400,300);
+ctx.strokeStyle = "#fa34a3";
+ctx.stroke();
